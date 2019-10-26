4cb417350debf8d5654316a00cda11ac78c48751
hiroshi sakamoto <metallicaslayer2002@yahioo.co.jp>
2019-10-26 18:26:28 +0900

    show.html img
    ヘッダー画像,showhtml,諸々調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all.order(created_at: 'DESC')
     end
+
+    def show
+        # @posts = Post.new
+        @posts = Post.find(params[:id])
+    end
 end
