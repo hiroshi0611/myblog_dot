18da4e41f14e511f8a3c0fd646d191d8eda4d5fc
hiroshi sakamoto <metallicaslayer2002@yahioo.co.jp>
2019-10-27 19:45:48 +0900

    validation createメソッド flash

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,24 @@ class PostsController < ApplicationController
         # @posts = Post.new
         @posts = Post.find(params[:id])
     end
+
+    def new
+        @posts = Post.new
+    end
+
+    def create
+        @posts = Post.new(post_params)
+        if @posts.save
+            flash[:success] = '登録成功しました'
+            redirect_to root_path
+        else
+          flash[:danger] = "入力してください"
+          render :new
+        end
+        
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
 end
